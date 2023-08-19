26a134580b4f0fdc772ce1c23171d6b15325e75d
foxching <rechielagria@gmail.com>
2023-08-19 17:46:13 +0800

    details page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DetailsComponent } from './components/details/details.component';
-import { DetailsmoviesComponent } from './components/detailsmovies/detailsmovies.component';
-import { DetailsreviewsComponent } from './components/detailsreviews/detailsreviews.component';
 import { NotAuthorizedComponent } from './components/not-authorized/not-authorized.component';
 import { SearchComponent } from './components/search/search.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { DetailsMoviesComponent } from './components/details-movies/details-movies.component';
+import { DetailsReviewsComponent } from './components/details-reviews/details-reviews.component';
+import { DetailsActorsComponent } from './components/details-actors/details-actors.component';
 
 @NgModule({
   declarations: [
@@ -20,12 +21,13 @@ import { ModalComponent } from './components/modal/modal.component';
     HomeComponent,
     FooterComponent,
     DetailsComponent,
-    DetailsmoviesComponent,
-    DetailsreviewsComponent,
     NotAuthorizedComponent,
     SearchComponent,
     LoaderComponent,
-    ModalComponent
+    ModalComponent,
+    DetailsMoviesComponent,
+    DetailsReviewsComponent,
+    DetailsActorsComponent
   ],
   imports: [
     BrowserModule
