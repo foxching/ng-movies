9606ada9e125adf67ca8653a4d602454cd946985
foxching <rechielagria@gmail.com>
2023-09-16 14:42:35 +0800

    Loading Search Results with HttpClient

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,18 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
-  movieTitle = ""
+  movieTitle = '';
+  searchResults: any[] = [];
 
-  constructor(private _activatedRoute: ActivatedRoute) { 
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private httpClient: HttpClient
+  ) {
     this._activatedRoute.params.subscribe((p) => {
-      this.movieTitle = p["movieTitle"];
-    })
+      this.movieTitle = p['movieTitle'];
+    });
+  }
+  ngOnInit(): void {
+    this.searchMovies();
   }
 
+  searchMovies() {
+    this.httpClient
+      .get<any[]>('assets/data/movieSearch.json')
+      .subscribe((data: any[]) => {
+        this.searchResults = data;
+      });
+  }
 }
